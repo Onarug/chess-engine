5e3dfd6a683ef9b52d3d8d70db0738d6f0c6db43
Onarug <anuraag.karunakaran@gmail.com>
2026-09-10 14:44:10 -0400

    Kign,pawn,knight attack tables have been created

--- chess.cpp
+++ chess.cpp
@@ -1,75 +1,22 @@
 #include <iostream>
 
 // Define
+// Primary variable for the bit borade 64 bits for 8 by 8 sqaure
 #define U64 unsigned long long
+
 
 // Enum board squares
 enum
 {
-    a8,
-    b8,
-    c8,
-    d8,
-    e8,
-    f8,
-    g8,
-    h8,
-    a7,
-    b7,
-    c7,
-    d7,
-    e7,
-    f7,
-    g7,
-    h7,
-    a6,
-    b6,
-    c6,
-    d6,
-    e6,
-    f6,
-    g6,
-    h6,
-    a5,
-    b5,
-    c5,
-    d5,
-    e5,
-    f5,
-    g5,
-    h5,
-    a4,
-    b4,
-    c4,
-    d4,
-    e4,
-    f4,
-    g4,
-    h4,
-    a3,
-    b3,
-    c3,
-    d3,
-    e3,
-    f3,
-    g3,
-    h3,
-    a2,
-    b2,
-    c2,
-    d2,
-    e2,
-    f2,
-    g2,
-    h2,
-    a1,
-    b1,
-    c1,
-    d1,
-    e1,
-    f1,
-    g1,
-    h1
+a8, b8, c8, d8, e8, f8, g8, h8,
+a7, b7, c7, d7, e7, f7, g7, h7,
+a6, b6, c6, d6, e6, f6, g6, h6,
+a5, b5, c5, d5, e5, f5, g5, h5,
+a4, b4, c4, d4, e4, f4, g4, h4,
+a3, b3, c3, d3, e3, f3, g3, h3,
+a2, b2, c2, d2, e2, f2, g2, h2,
+a1, b1, c1, d1, e1, f1, g1, h1,
+
 };
 
 /*
@@ -90,6 +37,70 @@ enum
     black
 };
 
+
+/*Not a file
+
+8 0 1 1 1 1 1 1 1
+7 0 1 1 1 1 1 1 1
+6 0 1 1 1 1 1 1 1
+5 0 1 1 1 1 1 1 1
+4 0 1 1 1 1 1 1 1
+3 0 1 1 1 1 1 1 1
+2 0 1 1 1 1 1 1 1
+1 0 1 1 1 1 1 1 1
+  a b c d e f g h
+
+Bitboard: 18374403900871474942
+Not h file
+8 1 1 1 1 1 1 1 0
+7 1 1 1 1 1 1 1 0
+6 1 1 1 1 1 1 1 0
+5 1 1 1 1 1 1 1 0
+4 1 1 1 1 1 1 1 0
+3 1 1 1 1 1 1 1 0
+2 1 1 1 1 1 1 1 0
+1 1 1 1 1 1 1 1 0
+  a b c d e f g h
+Bitboard: 9187201950435737471
+
+not hg
+
+8 1 1 1 1 1 1 0 0
+7 1 1 1 1 1 1 0 0
+6 1 1 1 1 1 1 0 0
+5 1 1 1 1 1 1 0 0
+4 1 1 1 1 1 1 0 0
+3 1 1 1 1 1 1 0 0
+2 1 1 1 1 1 1 0 0
+1 1 1 1 1 1 1 0 0
+  a b c d e f g h
+Bitboard: 4557430888798830399
+
+8 0 0 1 1 1 1 1 1
+7 0 0 1 1 1 1 1 1
+6 0 0 1 1 1 1 1 1
+5 0 0 1 1 1 1 1 1
+4 0 0 1 1 1 1 1 1
+3 0 0 1 1 1 1 1 1
+2 0 0 1 1 1 1 1 1
+1 0 0 1 1 1 1 1 1
+  a b c d e f g h
+Bitboard: 18229723555195321596
+
+
+*/
+const U64 not_a_file = 18374403900871474942ULL;
+
+const U64 not_h_file = 9187201950435737471ULL;
+
+const U64 not_hg_file = 4557430888798830399ULL;
+
+const U64 not_ab_file = 18229723555195321596ULL;
+
+
+void print_bitboard(U64 bitboard);
+
+
 // Bit Macros //
 // Get bit
 #define get_bit(bitboard, square) (bitboard & (1ULL << square))
@@ -101,42 +112,26 @@ enum
 // pawn attacks table
 U64 pawn_attacks[2][64];
 
-// generate pawn attacks
-U64 mask_pawn_attacks(int square, int side)
-{
-    // piece bit board
-    U64 bitboard = 0ULL;
-    // results attack bitboards
-    U64 attacks = 0ULL;
+// knight attacks table
+U64 knight_attacks[64];
 
-    // set piece on board
-    set_bit(bitboard, square);
+// king attacks table
+U64 king_attacks[64];
 
-    // white
-    if (!side)
-    {
 
-    } else {
-    // black
-
-    }
-
-    // return attack map
-    return attacks;
-}
 
 void getChessSquares()
 {
     for (int rank = 8; rank >= 1; rank--)
     {
-        std::cout << "'" << "a" << rank << "', ";
-        std::cout << "'" << "b" << rank << "', ";
-        std::cout << "'" << "c" << rank << "', ";
-        std::cout << "'" << "d" << rank << "', ";
-        std::cout << "'" << "e" << rank << "', ";
-        std::cout << "'" << "f" << rank << "', ";
-        std::cout << "'" << "g" << rank << "', ";
-        std::cout << "'" << "h" << rank << "', \n";
+        std::cout << "" << "a" << rank << ", ";
+        std::cout << "" << "b" << rank << ", ";
+        std::cout << "" << "c" << rank << ", ";
+        std::cout << "" << "d" << rank << ", ";
+        std::cout << "" << "e" << rank << ", ";
+        std::cout << "" << "f" << rank << ", ";
+        std::cout << "" << "g" << rank << ", ";
+        std::cout << "" << "h" << rank << ", \n";
     }
 }
 // Print bitboard
@@ -170,9 +165,114 @@ void print_bitboard(U64 bitboard)
     std::cout << "\n\n";
 }
 
-int main()
+// generate pawn attacks
+U64 mask_pawn_attacks(int side,int square)
 {
+    // piece bit board
+    U64 bitboard = 0ULL;
+    // results attack bitboards
+    U64 attacks = 0ULL;
+
+    // set piece on board
+    set_bit(bitboard, square);
+    // white
+    if (!side)
+    {
+        if((bitboard >> 7 ) & not_a_file){attacks |= (bitboard >> 7 );}
+        
+        if((bitboard >> 9 ) & not_h_file){attacks |= (bitboard >> 9 );}
         
 
+    } else {
+    // black
+        if((bitboard << 7 ) & not_h_file){attacks |= (bitboard << 7 );}
+        if((bitboard << 9 ) & not_a_file){attacks |= (bitboard << 9 );}
+
+        
+    }
+
+    // return attack map
+    //print_bitboard(attacks);
+    return attacks;
+}
+U64 mask_knight_attacks(int square){
+    // 17 15,10,6
+    U64 attacks = 0ULL;
+    U64 bitboard = 0ULL;
+    set_bit(bitboard, square);
+    if((bitboard << 17 ) & not_a_file){attacks |= (bitboard << 17);}
+    if((bitboard << 15 ) & not_h_file){attacks |= (bitboard << 15);}
+    if((bitboard << 10 ) & not_ab_file){attacks |= (bitboard << 10);}
+    if((bitboard << 6 ) & not_hg_file){attacks |= (bitboard << 6);}
+
+    if((bitboard >> 17 ) & not_h_file){attacks |= (bitboard >> 17);}
+    if((bitboard >> 15 ) & not_a_file){attacks |= (bitboard >> 15);}
+    if((bitboard >> 10 ) & not_hg_file){attacks |= (bitboard >> 10);}
+    if((bitboard >> 6 ) & not_ab_file){attacks |= (bitboard >> 6);}
+
+
+
+    return attacks;
+}
+
+// masj king attack
+
+U64 mask_king_attacks(int square){
+    // 1 , 8, 7,9
+    U64 attacks = 0ULL;
+    U64 bitboard = 0ULL;
+    set_bit(bitboard, square);
+
+    attacks |= (bitboard << 8);
+    attacks |= (bitboard >> 8);
+
+    if((bitboard >> 9) & not_h_file){attacks |= (bitboard >> 9);}
+    if((bitboard << 9) & not_a_file){attacks |= (bitboard << 9);}
+
+    if((bitboard >> 7) & not_a_file){attacks |= (bitboard >> 7);}
+    if((bitboard << 7) & not_h_file){attacks |= (bitboard << 7);}
+
+    if((bitboard >> 1) & not_h_file){attacks |= (bitboard >> 1);}
+    if((bitboard << 1) & not_a_file){attacks |= (bitboard << 1);}
+ 
+    return attacks;    
+
+
+}
+
+//Generate Pawn Attacks
+void init_pawn_attacks(){
+    for (int square = 0; square < 64; square++){
+        pawn_attacks[white][square] = mask_pawn_attacks(white,square);
+        pawn_attacks[black][square] = mask_pawn_attacks(black,square);
+
+
+    }
+}
+
+void init_knight_attacks(){
+    for (int square = 0; square <64; square++){
+        knight_attacks[square]= mask_knight_attacks(square);
+    }
+}
+
+void init_king_attacks(){
+    for(int square = 0; square < 64; square++){
+        knight_attacks[square] = mask_king_attacks(square);
+        print_bitboard(knight_attacks[square]);
+    }   
+}
+
+void init_leaper_attacks(){
+    init_king_attacks();
+    init_knight_attacks();
+    init_pawn_attacks();
+}
+
+int main()
+{
+    U64 bit_board = 0ULL;
+    init_leaper_attacks();
+    //print_bitboard(mask_king_attacks(a1));
     return 0;
 }
